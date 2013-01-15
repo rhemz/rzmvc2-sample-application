21967030024201ee8a19691eab1b6e4b22b9ff26
Russ Zeien <russ@zeien.org>
2013-01-15 18:32:32 -0500

    sample app config initial commit

---
+++ config/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+	Wildcard:
+		%var
+
+	Argument access:
+		$n   i.e. $1, $2, $3, etc
+
+	Ex: 
+		"/xx/%var/%var/%var"	=> '/test/index/$1/$2/$3',
+*/
+
+$config['mappings'] = array(
+	
+	"/login"								=> '/account/login',
+	"/logout"								=> '/account/logout'
+	
+);
